b4536643aa5ccfff7e19006158eb708182d218c8
Jack <xiaozhi.1228@outlook.com>
2024-10-17 18:32:24 +0800

    somgthing changed

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -131,7 +131,7 @@ public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
             return false;
         }
         for (int i = 0; i < size; i++) {
-            if (!(lld.get(i).equals(get(i)))) {
+            if (lld.get(i) != get(i)) {
                 return false;
             }
         }
